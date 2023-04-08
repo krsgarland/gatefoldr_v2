0dc8625a28a5afbad40483d8fb6867a0bcf106d4
krsgarland <97922634+krsgarland@users.noreply.github.com>
2023-04-08 17:40:19 -0500

    added sign up route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :categories
  root to: "pages#home"
  get '/about-us', to: "pages#about_us"
+ get '/sign-up', to: "users#new"
+ resources :users, except: [:new|]
+ resources :categories
  resources :albums
 end
