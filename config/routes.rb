7c26536d5e946f49fb48d80379b659d86c507f07
krsgarland <97922634+krsgarland@users.noreply.github.com>
2023-04-08 17:49:55 -0500

    added user controller, sign up route and form

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
  root to: "pages#home"
  get '/about-us', to: "pages#about_us"
  get '/sign-up', to: "users#new"
- resources :users, except: [:new|]
+ resources :users, except: [:new]
  resources :categories
  resources :albums
 end
+
