732a829151207d98dd9617a32e68c81f64a87b8d
krsgarland <97922634+krsgarland@users.noreply.github.com>
2023-04-08 18:49:29 -0500

    added login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
  root to: "pages#home"
  get '/about-us', to: "pages#about_us"
  get '/sign-up', to: "users#new"
+ get '/login', to: "sessions#new"
+ post '/login', to "sessions#create"
  resources :users, except: [:new]
  resources :categories
  resources :albums
