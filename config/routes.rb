8836ead5f46632f2ec726e6dfa17f64b7a972e0a
krsgarland <97922634+krsgarland@users.noreply.github.com>
2023-04-10 11:13:13 -0500

    added login page & sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
  root to: "pages#home"
  get '/about-us', to: "pages#about_us"
  get '/sign-up', to: "users#new"
  get '/login', to: "sessions#new"
- post '/login', to "sessions#create"
+ post '/login', to: "sessions#create"
  resources :users, except: [:new]
  resources :categories
  resources :albums
