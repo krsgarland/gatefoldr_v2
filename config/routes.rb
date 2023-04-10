71874b36dcefa385213e8e1df4571a174b5f2d6d
krsgarland <97922634+krsgarland@users.noreply.github.com>
2023-04-10 12:33:18 -0500

    added logout route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
  get '/sign-up', to: "users#new"
  get '/login', to: "sessions#new"
  post '/login', to: "sessions#create"
+ delete '/logout', to: "sessions#destroy"
  resources :users, except: [:new]
  resources :categories
  resources :albums
