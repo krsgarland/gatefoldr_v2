8c38467e490234a05d8d470e931c0ed144ada557
krsgarland <97922634+krsgarland@users.noreply.github.com>
2023-04-10 12:49:50 -0500

    added logout visibility to navbar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -21,6 +21,6 @@ class SessionsController < ApplicationController
   def destroy 
     session[:user_id] = nil
     flash[:notice] = "Logout successful!"
-    redirect_to login_path
+    redirect_to root_path
   end
 end
