ed7101a94937b95b5ae366212db289ce7b76e112
krsgarland <97922634+krsgarland@users.noreply.github.com>
2023-04-10 11:20:54 -0500

    added functionality to login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+      flash[:notice] = "Login Successful!"
+      redirect_to albums_path
+
+    else
+      flash[:notice] = "Login Failed."
+      render :new, status: :unprocessable_entity
+
+    end
   end
 end
